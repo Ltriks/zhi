cf24bd6346e6a80197558ef3551d2d8607c37e88
lqtriks_homestead <lqtriks@gmail.com>
2017-07-11 05:49:17 +0000

    16

--- app/User.php
+++ app/User.php
@@ -19,6 +19,11 @@ class User extends Authenticatable
         'name', 'email', 'password', 'avatar', 'confirmation_token',
     ];
 
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+
     /**
      * The attributes that should be hidden for arrays.
      *
