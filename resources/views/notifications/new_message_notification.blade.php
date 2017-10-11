f0c35da4473b2bde0f51cfb2c066ebafa7745df6
trick <lqtriks@gmail.com>
2017-10-11 07:09:18 +0000

    41

--- resources/views/notifications/new_message_notification.blade.php
+++ resources/views/notifications/new_message_notification.blade.php
@@ -1,3 +1,3 @@
-<li class="notifications">
-    <a href="/inbox/{{ $notification->data['dialog'] }}">{{ $notification->data['name'] }} texted YOU.</a>
+<li class="notifications {{ $notification->unread() ? 'unread' : ' ' }}">
+    <a href="{{ $notification->unread() ? '/notifications/'.$notification->id.'?redirect_url=/inbox/'.$notification->data['dialog'] : '/inbox/'.$notification->data['dialog'] }}">{{ $notification->data['name'] }} texted YOU.</a>
 </li>
